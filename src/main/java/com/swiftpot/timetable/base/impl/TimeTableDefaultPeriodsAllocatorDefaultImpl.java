862ebd451762fddebf4a99ccddb5939e941f94d7
Ace P Rbk <rbk.unlimited@gmail.com>
2016-12-22 14:22:41 +0000

    TimeTableDefaultPeriodsAllocatorDefaultImpl part implementatiion

--- src/main/java/com/swiftpot/timetable/base/impl/TimeTableDefaultPeriodsAllocatorDefaultImpl.java
+++ src/main/java/com/swiftpot/timetable/base/impl/TimeTableDefaultPeriodsAllocatorDefaultImpl.java
@@ -1,8 +1,13 @@
 package com.swiftpot.timetable.base.impl;
 
 import com.swiftpot.timetable.base.TimeTableDefaultPeriodsAllocator;
+import com.swiftpot.timetable.model.ProgrammeDays;
+import com.swiftpot.timetable.model.ProgrammeGroup;
+import com.swiftpot.timetable.model.YearGroup;
 import com.swiftpot.timetable.repository.db.model.TimeTableSuperDoc;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author Ace Programmer Rbk
@@ -13,6 +18,22 @@ import org.springframework.stereotype.Component;
 public class TimeTableDefaultPeriodsAllocatorDefaultImpl implements TimeTableDefaultPeriodsAllocator {
     @Override
     public TimeTableSuperDoc allocateDefaultPeriodsOnTimeTable(TimeTableSuperDoc timeTableSuperDocWithInitialDefaultDataSet) {
+        List<YearGroup> yearGroupList = timeTableSuperDocWithInitialDefaultDataSet.getYearGroupsList();
+        int numberOfYearGroups = yearGroupList.size();
+        for (int currentNo = 0; currentNo < numberOfYearGroups; currentNo++) {
+            //now we need to get all ProgrammeGroups in yearGroupList
+            List<ProgrammeGroup> programmeGroupList = yearGroupList.get(currentNo).getProgrammeGroupList();
+            int numberOfProgrammeGroups = programmeGroupList.size();
+            for (int currentProgrammeGroup = 0; currentProgrammeGroup < numberOfProgrammeGroups; currentProgrammeGroup++) {
+                //now we need to get all the ProgrammeDays as list
+                List<ProgrammeDays> programmeDaysList = programmeGroupList.get(currentProgrammeGroup).getProgrammeDaysList();
+                int numberOfProgrammeDays = programmeDaysList.size();
+                for (int currentProgrammeDay = 0; currentProgrammeDay < numberOfProgrammeDays; currentProgrammeDay++) {
+
+                }
+            }
+        }
+
         return null;
     }
 }
