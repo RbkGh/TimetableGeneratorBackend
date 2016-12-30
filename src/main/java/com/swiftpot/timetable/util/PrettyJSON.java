32c4415a91669ea7f67e91ca60dbb9cb5605fd87
Ace P Rbk <rbk.unlimited@gmail.com>
2016-12-30 03:14:16 +0000

    Added Pretty JSON class for readable json in console

---
+++ src/main/java/com/swiftpot/timetable/util/PrettyJSON.java
@@ -0,0 +1,30 @@
+package com.swiftpot.timetable.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * @author Ace Programmer Rbk
+ *         <Rodney Kwabena Boachie at [rodney@swiftpot.com,rbk.unlimited@gmail.com]> on
+ *         29-Dec-16 @ 7:12 PM
+ */
+public final class PrettyJSON {
+
+    /**
+     * Convert a JSON string to pretty print version
+     *
+     * @param jsonString
+     * @return
+     */
+    public static String toPrettyFormat(String jsonString) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(jsonString).getAsJsonObject();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String prettyJson = gson.toJson(json);
+
+        return prettyJson;
+    }
+}
