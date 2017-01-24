6d22f98f0659942780a0481b183e71ca9ceb38b2
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-24 15:11:09 +0000

    Added SubjectDocWithExtraInfo model class,and changed return type of getAllSubjectAllocationState endpoint

--- src/main/java/com/swiftpot/timetable/controllers/SubjectsAllocationController.java
+++ src/main/java/com/swiftpot/timetable/controllers/SubjectsAllocationController.java
@@ -2,6 +2,7 @@ package com.swiftpot.timetable.controllers;
 
 import com.swiftpot.timetable.model.ErrorOutgoingPayload;
 import com.swiftpot.timetable.model.OutgoingPayload;
+import com.swiftpot.timetable.model.SubjectDocWithExtraInfo;
 import com.swiftpot.timetable.model.SuccessfulOutgoingPayload;
 import com.swiftpot.timetable.repository.SubjectAllocationDocRepository;
 import com.swiftpot.timetable.repository.SubjectDocRepository;
@@ -43,6 +44,48 @@ public class SubjectsAllocationController {
         }
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public OutgoingPayload getSubjectAllocationDocsForSubject(@PathVariable String id) {
+        System.out.println("request get SubjectAllocationDocs for subject recieved");
+        if (subjectDocRepository.exists(id)) {
+            SubjectDoc subjectDoc = subjectDocRepository.findOne(id);
+            List<SubjectAllocationDoc> subjectAllocationDocs = subjectAllocationDocRepository.findBySubjectCode(subjectDoc.getSubjectCode());
+            return new SuccessfulOutgoingPayload(subjectAllocationDocs);
+        } else {
+            return new ErrorOutgoingPayload("Subject does not exit");
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/multiple", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public OutgoingPayload updateMultipleSubjectsAllocation(@RequestBody List<SubjectAllocationDoc> subjectAllocationDocs) {
+        OutgoingPayload outgoingPayloadResponse;
+        ArrayList<Boolean> successfulSaveResponses = new ArrayList<>();
+        List<SubjectAllocationDoc> savedSubjectAllocationDocs = new ArrayList<>();
+        subjectAllocationDocs.forEach((subjectAllocationDoc) -> {
+            if (subjectAllocationDocRepository.findBySubjectCodeAndYearGroup(subjectAllocationDoc.getSubjectCode(), subjectAllocationDoc.getYearGroup()) != null) {
+                SubjectAllocationDoc existingSubDoc = subjectAllocationDocRepository.findBySubjectCodeAndYearGroup(subjectAllocationDoc.getSubjectCode(), subjectAllocationDoc.getYearGroup());
+                subjectAllocationDoc.setId(existingSubDoc.getId());
+                SubjectAllocationDoc subjectAllocationDocSaved = subjectAllocationDocRepository.save(subjectAllocationDoc);
+                if (subjectAllocationDocSaved.getId() != null) {
+                    successfulSaveResponses.add(true); //once there is an id field,it was saved
+                    savedSubjectAllocationDocs.add(subjectAllocationDocSaved);
+                } else {
+                    successfulSaveResponses.add(false);//could not save since there is no id
+                }
+
+            } else {
+                successfulSaveResponses.add(false);//does not even exist,add false
+            }
+        });
+
+        if (successfulSaveResponses.contains(false)) {
+            outgoingPayloadResponse = new ErrorOutgoingPayload("Not all were saved.Retry ");
+        } else {
+            outgoingPayloadResponse = new SuccessfulOutgoingPayload(successfulSaveResponses);
+        }
+        return outgoingPayloadResponse;
+    }
+
     /**
      * if one subjectAllocationDoc is not set,then set {SubjectDoc's isAllSubjectYearGroupsAllocated property} to false
      *
@@ -55,6 +98,7 @@ public class SubjectsAllocationController {
         if (!(subjectDocs.size() > 0)) {
             return new SuccessfulOutgoingPayload(subjectDocs);
         } else {
+
             subjectDocs.forEach((subjectDoc -> {
                 //find all subjectAllocationDocs by subjectCode
                 List<SubjectAllocationDoc> subjectAllocationDocs = subjectAllocationDocRepository.findBySubjectCode(subjectDoc.getSubjectCode());
@@ -70,16 +114,25 @@ public class SubjectsAllocationController {
                         //totalSubjectAllocation is not 0 so add true since it's allocated
                         booleanArrayList.add(true);
                     }
-                    //now check if false exists in the list,then render subjectDoc's isAllSubjectYearGroupsAllocated as false
-                    if (booleanArrayList.contains(false)) {
-                        subjectDoc.setIsAllSubjectYearGroupsAllocated(false);
-                    } else {
-                        subjectDoc.setIsAllSubjectYearGroupsAllocated(true);
-                    }
                 });
+                //now check if false exists in the list,then render subjectDoc's isAllSubjectYearGroupsAllocated as false
+                if (booleanArrayList.contains(false)) {
+                    subjectDoc.setIsAllSubjectYearGroupsAllocated(false);
+                } else {
+                    subjectDoc.setIsAllSubjectYearGroupsAllocated(true);
+                }
             }));
+
         }
 
-        return new SuccessfulOutgoingPayload(subjectDocs);
+        List<SubjectDocWithExtraInfo> subjectDocWithExtraInfosList = new ArrayList<>();
+        subjectDocs.forEach((subjectDoc) -> {
+            //find all subjectAllocationDocs by subjectCode
+            List<SubjectAllocationDoc> subjectAllocationDocs = subjectAllocationDocRepository.findBySubjectCode(subjectDoc.getSubjectCode());
+            subjectDocWithExtraInfosList.add(new SubjectDocWithExtraInfo(subjectDoc, subjectAllocationDocs));
+        });
+
+
+        return new SuccessfulOutgoingPayload(subjectDocWithExtraInfosList);
     }
 }
